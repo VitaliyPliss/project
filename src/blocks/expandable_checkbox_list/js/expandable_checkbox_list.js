754cfb5713a22314098c7e15e2df4c90efa778da
VitaliiPliss <v.pliss@mail.ru>
2021-05-12 18:26:05 +0300

    expandable_checkbox_done

--- src/blocks/expandable_checkbox_list/js/expandable_checkbox_list.js
+++ src/blocks/expandable_checkbox_list/js/expandable_checkbox_list.js
@@ -1,10 +1,12 @@
 let flag1 = false;
   $('.expcheckbox-buttons').click(function () {
+    $('.expcheckbox-buttons').toggleClass('rotate180');
+    
     if(!flag1){
      $('.expcheckbox-buttons-wrapper').slideDown();
+
     }else{
      $('.expcheckbox-buttons-wrapper').slideUp();
     }
     flag1 = !flag1;
   });
-
